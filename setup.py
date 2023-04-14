e626f7980dca5e6aa03de8311c66b2eda3228d2b
deepak-sharma-ta <deepak.sharma@tigeranalytics.com>
2023-04-14 15:15:11 +0530

    changes to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="table_summary",
     version="0.0.1",
-    # package_dir={"": "table_summarizer"},
+    package_dir={"": "table_summary"},
     install_requires=[
         "datasets",
         "evaluate",
